61fb53a0e0b8ec7b9279e08527bd43a4bbd43325
Anditya Arifianto <anditya@telkomuniversity.ac.id>
2019-11-02 20:12:20 +0700

    Update optimizer.py

--- vis/optimizer.py
+++ vis/optimizer.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 import numpy as np
+import tensorflow as tf
 from tensorflow.keras import backend as K
 
 from .callbacks import Print
@@ -41,7 +42,7 @@ class Optimizer(object):
         self.wrt_tensor = self.input_tensor if wrt_tensor is None else wrt_tensor
         if self.input_tensor is self.wrt_tensor:
             self.wrt_tensor_is_input_tensor = True
-            self.wrt_tensor = K.identity(self.wrt_tensor)
+            self.wrt_tensor = tf.identity(self.wrt_tensor)
         else:
             self.wrt_tensor_is_input_tensor = False
 
